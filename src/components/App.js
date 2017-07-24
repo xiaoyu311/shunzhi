0475a6beef618ec92cee98a0d7861c11dfc7e288
xiaoyu311 <616872074@qq.com>
2017-07-25 01:06:36 +0800

    little

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import LoginPage from './LoginPage'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ShunZhi from './ShunZhi'
 import SignupPage from './SignupPage'
-import { nameOfAnimation as Menu } from 'react-burger-menu'
 
 
 
@@ -17,9 +16,6 @@ class App extends React.Component{
               <Route path="/login" component={LoginPage} />
               <Route path="/signup" component={SignupPage} />
             </Switch>
-            <Menu pageWrapId={ "LoginPage" }>
-              {/* <span></span> */}
-            </Menu>
           </div>
       </BrowserRouter>
     )
